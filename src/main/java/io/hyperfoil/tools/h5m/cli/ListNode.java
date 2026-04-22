5bb4662ad895959b40dea08ad79ad84f73f225c1
Will Reichert <willr3@gmail.com>
2026-04-22 10:30:07 -0400

    add 'as Graph|Table' to list nodes

--- src/main/java/io/hyperfoil/tools/h5m/cli/ListNode.java
+++ src/main/java/io/hyperfoil/tools/h5m/cli/ListNode.java
@@ -3,12 +3,19 @@ package io.hyperfoil.tools.h5m.cli;
 import io.hyperfoil.tools.h5m.api.Node;
 import io.hyperfoil.tools.h5m.api.NodeGroup;
 import io.hyperfoil.tools.h5m.api.svc.NodeGroupServiceInterface;
+import io.hyperfoil.tools.h5m.entity.NodeEntity;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.aesh.util.graph.GraphStyle;
 import picocli.CommandLine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
+
+import org.aesh.util.graph.Graph;
+import org.aesh.util.graph.GraphNode;
 
 @CommandLine.Command(name="nodes", separator = " ", description = "list nodes", mixinStandardHelpOptions = true)
 public class ListNode implements Callable<Integer> {
@@ -19,7 +26,11 @@ public class ListNode implements Callable<Integer> {
     @Inject
     NodeGroupServiceInterface nodeGroupService;
 
+    public static enum Render {Table, Graph};
+
     @CommandLine.Option(names = {"from"},description = "group name", arity="0..1") String groupName;
+
+    @CommandLine.Option(names = {"as"}, description = "Valid values: ${COMPLETION-CANDIDATES}\")", defaultValue = "Table") Render render;
 
     @Override
     @Transactional
@@ -35,18 +46,41 @@ public class ListNode implements Callable<Integer> {
             System.err.println("NodeEntity group "+groupName+" not found");
             return 1;
         }
-        System.out.println(
-            ListCmd.table(80,nodeGroup.sources(),List.of("name","type","fqdn","operation"),
-                List.of(Node::name,
-                n->n.type().display(),
-                Node::fqdn,
-                Node::operation
+        if(render.equals(Render.Graph)){
+            GraphNode rootNode = GraphNode.of("root");
+            Map<Node,GraphNode> nodes = new HashMap<>();
+            nodes.put(nodeGroup.root(), rootNode);
+                    for(Node source: nodeGroup.sources()){
+                    walk(source,nodes);
+                }
+            System.out.println(Graph.render(rootNode, GraphStyle.ROUNDED));
+        }else {
+            System.out.println(
+                ListCmd.table(80,nodeGroup.sources(),List.of("name","type","fqdn","operation"),
+                    List.of(Node::name,
+                        n->n.type().display(),
+                        Node::fqdn,
+                        Node::operation
+                    )
                 )
-            )
-        );
+            );
+        }
+
         return 0;
     }
 
-
+    public GraphNode walk(Node node, Map<Node,GraphNode> nodes){
+        if(nodes.containsKey(node)){
+            return nodes.get(node);
+        }else{
+            GraphNode rtrn = GraphNode.of(node.name());
+            nodes.put(node, rtrn);
+            for(Node source: node.sources()){
+                    GraphNode fromSource = walk(source,nodes);
+                    fromSource.child(rtrn);
+                }
+            return rtrn;
+        }
+    }
 
 }
